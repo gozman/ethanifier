75812ffb8244e00462827bd1d83add033d5d68f1
mike gozzo <mgozzo@gmail.com>
2014-10-31 14:30:57 -0400

    Ask Smirnov

--- Ethanifier/AppDelegate.m
+++ Ethanifier/AppDelegate.m
@@ -19,7 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     //TODO: Replace with an app token you generate for free at http://www.supportkit.io
-    SKTSettings* settings = [SKTSettings settingsWithAppToken:@"YOUR-APP-TOKEN"];
+    SKTSettings* settings = [SKTSettings settingsWithAppToken:@"0a654c4nnwd6bhrdf8gdjemob"];
     settings.enableAppWideGesture = NO;
     settings.enableGestureHintOnFirstLaunch = NO;
 
